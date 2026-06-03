fa9069e91e378807bba87aedb24cf559835df683
senmdaniel <senmdaniel@hotmail.com>
2026-06-03 18:04:10 +0200

    Update config_flow.py

--- custom_components/zmanim_pro/config_flow.py
+++ custom_components/zmanim_pro/config_flow.py
@@ -1,18 +1,24 @@
+import logging
+import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 class ZmanimProConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Beheert de configuratie-flow voor Zmanim Pro."""
+    
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Eerste stap wanneer een gebruiker de integratie toevoegt."""
+        """Eerste stap wanneer een gebruiker de integratie toevoegt via de UI."""
         # Controleer of de integratie niet al een keer is toegevoegd
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
+        # Als de gebruiker op 'Indienen' klikt in het lege pop-up scherm:
         if user_input is not None:
-            # Maak de integratie direct aan in de UI
             return self.async_create_entry(title="Zmanim Pro", data={})
 
-        return self.async_show_form(step_id="user")
+        # Toon een simpel leeg formulier (geen invoervelden nodig, we gebruiken de HA-locatie)
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
