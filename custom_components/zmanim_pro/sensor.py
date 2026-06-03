3822d798f0a8c5bec875832965899e0f9f2417a0
senmdaniel <senmdaniel@hotmail.com>
2026-06-03 15:16:25 +0200

    Add JewishHolidaySensor to Zmanim Pro sensors

--- custom_components/zmanim_pro/sensor.py
+++ custom_components/zmanim_pro/sensor.py
@@ -4,21 +4,22 @@ from homeassistant.helpers.entity import Entity
 # Importeer je eigen DateProvider
 from .date_provider import DateProvider
 
-# Importeer de sensoren uit de aparte bestanden
+# Importeer de sensoren uit de losse bestanden
 from .sensors_data import GregorianDateSensor, JewishDateSensor
-# (Straks voegen we hier sensors_tijden en sensors_feestdagen aan toe!)
+from .sensors_feestdagen import JewishHolidaySensor
 
-# Update de sensoren elke 30 minuten
+# Update de sensoren elke 30 minutes
 SCAN_INTERVAL = timedelta(minutes=30)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Zet alle Zmanim Pro sensoren klaar."""
     provider = DateProvider()
     
-    # Hier verzamelen we alle sensoren uit de losse bestanden
+    # We voegen de nieuwe JewishHolidaySensor toe aan de actieve lijst
     lijst_van_sensoren = [
         GregorianDateSensor(provider),
-        JewishDateSensor(provider)
+        JewishDateSensor(provider),
+        JewishHolidaySensor(provider)
     ]
     
     # Geef de hele lijst in één keer aan Home Assistant
